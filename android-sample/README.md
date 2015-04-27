d2709e45f37c29d88bbe682eae4fd1070810caff
Mark <mark11yan@gmail.com>
2015-04-27 17:23:15 +0800

    only outfit

---
+++ android-sample/README.md
@@ -0,0 +1,4 @@
+
+This project demonstrates a simple Android app crearted by
+Android Studio. The project also includes an example to 
+show how to use dependency injection.
---
+++ android-sample/app/src/main/java/ExampleApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import android.app.Application;
+
+public class ExampleApp extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+
+}
